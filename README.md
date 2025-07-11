40b7ac8deefc01b4f4b35373d5be1e7f1dd2b9c8
David Vargas <davidvargas1224@gmail.com>
2025-07-11 18:36:38 -0500

    Entrenamiento4

---
+++ README.md
@@ -0,0 +1,85 @@
+README - Training – Module 3 Week 3
+
+CRUD
+
+Project Description
+
+This project is a web page built using JavaScript, HTML/CSS, and JSON Server to allows you to apply practical knowledge of DOM manipulation and browser storage. The web page provides a simple, user-friendly interface where:
+
+- Captures the user's name and age through input fields.
+- Validates users inputs (name and age).
+- Stores and display data using `localStorage` and `sessionStorage`
+- Persists data across page reloads.
+
+The system validates inputs to ensure clean data: only letters are allowed in names and only numbers in age.
+Upon successful validation, the user's name and age are saved in the browser’s localStorage under the key 'usuarios' as part of an array of user objects. This approach allows multiple entries to be stored during the same session. The saved data persists across page reloads and browser restarts, and it is dynamically displayed on the page whenever it loads. In addition to local storage, the application tracks user interactions using sessionStorage. Every time the "Save" button is clicked, a counter is incremented and displayed on the interface, showing the number of interactions during the current session. This counter is maintained as long as the browser tab remains open but resets automatically when the tab is closed. The interface also includes a "Clear" button that removes all stored user data from localStorage and updates the display accordingly. However, it does not reset the session counter unless the session is closed. Furthermore, user data is also sent to a JSON server using the Fetch API.
+
+
+Requirements
+
+- Node.js installed including Node Package Manager
+- JSON server installed
+- Any code editor (e.g., VS Code)
+- Internet connection for installing packages
+
+How to use it
+
+1. Clone the project
+git clone https://github.com/githubegg12/Module3Week3.git
+
+2. Install dependencies
+npm i
+npm install json-server
+
+
+
+3. Start the development servers
+
+You will need two terminals:
+
+Terminal 1 – Start frontend with Vite
+npm run dev
+Visit: http://localhost:5173
+
+Terminal 2 – Start backend with JSON Server
+npx run server public/Json/db.js
+Visit: http://localhost:3000/usuarios
+
+4. Enter a valid name and age in the form.
+
+5. Click the "Save" button to store the information.
+The data will be displayed on the page, and the interaction counter will increase.
+
+6. Click the "Clear" button to remove all stored data and reset the counter.
+
+
+Project structure
+
+
+/Entrenamiento4
+├── /public
+|   └──/Json
+│      └── db.json #JSON Server database
+├── index.html #Main HTML UI
+├── package.json
+├── vite.config.js
+├── /src
+│   ├── /CSS
+│   │   └── styles.css  #Styling for the application
+│   └── /JavaScript
+│       └── main.js #Main logic for web page
+
+
+
+
+
+Coder information
+
+Nombre: David Felipe Vargas Varela	
+
+Clan: Sierra
+
+Correo: davidvargas1224@gmail.com
+
+
+ 
---
+++ src/JavaScript/main.js
@@ -0,0 +1,121 @@
+
+// To reference the elements
+const form = document.getElementById('userForm');
+const output = document.getElementById('output');
+const counter = document.getElementById('counter');
+const saveBtn = document.getElementById('saveBtn');
+const clearBtn = document.getElementById('clearBtn');
+const serverOutput = document.getElementById('serverOutput');
+
+// To load the page with the information 
+window.addEventListener('load', () => {
+  displayData();
+  showCounter();
+  loadFromServer();
+});
+
+// Save data from the form
+saveBtn.addEventListener('click', async (event) => {
+  event.preventDefault();
+
+  const nameInput = document.getElementById('name').value.trim();
+  const ageInput = document.getElementById('age').value.trim();
+
+
+  try {
+    // Validate users to fulfill the form
+    if (nameInput === '' || ageInput === '') {
+      alert('Faltan campos por llenar');
+      return;
+    }
+
+    if (!/^[a-zA-Z\s]+$/.test(nameInput)) {
+      throw new Error("El nombre debe contener solo letras.");
+    }
+    //Validate if ages is a valid number
+    if (isNaN(ageInput) || ageInput <= 0 || ageInput >= 150) {
+      throw new Error("La edad debe ser un numero valido entre 1 y 150");
+    }
+
+    const data = { nameInput, ageInput };
+    let usuarios = JSON.parse(localStorage.getItem('usuarios')) || []; 
+    usuarios.push(data); // To add user information on the list
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));// To update the list of users added in a single session
+    displayData();
+
+    const response = await fetch('http://localhost:3000/usuarios', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: nameInput, age: ageInput })
+    });
+
+    if (!response.ok) throw new Error('No se pudo guardar en el servidor.');
+    loadFromServer();
+
+
+  } catch (error) {
+    console.error(error);
+    alert(error.message);
+  }
+});
+
+// To show users saved during one session
+
+function displayData() {
+  const savedData = localStorage.getItem('usuarios');
+
+  if (savedData) {
+    const usuarios = JSON.parse(savedData);
+    output.innerHTML = usuarios
+      .map(data => `Nombre: ${data.nameInput} | Edad: ${data.ageInput}`)
+      .join('<br>');
+  } else {
+    output.textContent = 'No hay datos almacenados, ingresa datos.';
+  }
+}
+
+// Show users from Json server
+async function loadFromServer() {
+  try {
+    const server = await fetch('http://localhost:3000/usuarios');
+    if (!server.ok) throw new Error('No se pudo obtener datos del servidor');
+
+    const data = await server.json();
+
+    if (data.length === 0) {
+      serverOutput.textContent = 'No hay usuarios en el servidor.';
+      return;
+    }
+    serverOutput.innerHTML = data.map(user => `${user.name} (${user.age} años)`).join('<br>');
+  } catch (error) {
+    console.error(error.message);
+    serverOutput.textContent = 'Error al cargar datos desde el servidor.';
+  }
+}
+
+// Counter to know the amount of interactions
+function updateCounter() {
+  let interactions = sessionStorage.getItem('contador');
+
+  if (!interactions) {
+    interactions = 1;
+  } else {
+    interactions = parseInt(interactions) + 1;
+  }
+  sessionStorage.setItem('contador', interactions);
+  counter.textContent = `Interacciones en esta sesión: ${interactions}`;
+}
+saveBtn.addEventListener('click', updateCounter);
+
+//To display the number of interactions
+function showCounter() {
+  let interactions = sessionStorage.getItem('contador') || 0;
+  counter.textContent = `Interacciones en esta sesión: ${interactions}`;
+}
+
+
+// Clean data button
+clearBtn.addEventListener('click', () => {
+  localStorage.clear();
+  displayData();
+});
